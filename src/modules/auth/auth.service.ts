0824f5b3b1e6d024d6cc58a7170c8730e36bad34
omarCelaya <omercadocelaya@gmail.com>
2026-02-10 21:15:31 -0700

    fix: auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,40 +1,41 @@
 import jwt, { SignOptions, Secret } from "jsonwebtoken";
-import type { StringValue } from "ms"
 import { supabase } from "../../utils/supabase.js";
 import { JwtPayload, LoginPayload } from "./auth.model.js";
 
 export const loginService = async (payload: LoginPayload) => {
-  try { 
-  const { email, password } = payload;
-  console.log(payload)
-  if (email || password) {
-    throw new Error("Not credentials");
-  }
-  const { data: sessionData, error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
-  console.log(error)
-  if (error || !sessionData.user) {
-    throw new Error("Invalid credentials", error);
-  }
+  try {
+    const { email, password } = payload;
 
-  // 3️⃣ Crear TU JWT
-  const newToken: JwtPayload = {
-    admin_id: sessionData.user.id,
-    email: sessionData.user.email!
-  };
+    if (!email || !password) {
+      throw new Error("Missing credentials");
+    }
 
-  const jwtSecret: Secret = process.env.JWT_SECRET as string;
-  const signOptions: SignOptions = {
-    expiresIn: 7
-  };
+    const { data: sessionData, error } =
+      await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
 
-  const token = jwt.sign(newToken, jwtSecret, signOptions);
+    if (error || !sessionData.user) {
+      throw new Error(error?.message || "Invalid credentials");
+    }
 
-  return { token };
-   } catch (error) {
-    console.log(error)
-        throw new Error("AUTH-SERIVCE ERROR: ", error);
+    const newToken: JwtPayload = {
+      admin_id: sessionData.user.id,
+      email: sessionData.user.email!,
+    };
+
+    const jwtSecret: Secret = process.env.JWT_SECRET as string;
+
+    const signOptions: SignOptions = {
+      expiresIn: "7d",
+    };
+
+    const token = jwt.sign(newToken, jwtSecret, signOptions);
+
+    return { token };
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
 };
